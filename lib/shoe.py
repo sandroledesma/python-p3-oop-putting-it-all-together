76324713d911cdc48eca9f59049a47dad4e5626f
Alessandro Ledesma <sandroledesma@gmail.com>
2024-06-05 17:33:24 -0400

    update files

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size=0, condition="New"):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, new_size):
+        if isinstance(new_size, int):
+            self._size = new_size
+        else: 
+            print("size must be an integer")
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+
+    
